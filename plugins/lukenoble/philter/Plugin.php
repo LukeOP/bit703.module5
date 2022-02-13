cc39bc3fa8d0ba861f8544d9ed7dfac5b03ed92d
Luke Noble <luke.noble91@hotmail.com>
2022-02-13 15:14:47 +1300

    Backend setup complete

--- plugins/lukenoble/philter/Plugin.php
+++ plugins/lukenoble/philter/Plugin.php
@@ -1,4 +1,6 @@
-<?php namespace LukeNoble\Philter;
+<?php
+
+namespace LukeNoble\Philter;
 
 use System\Classes\PluginBase;
 
@@ -6,6 +8,14 @@ class Plugin extends PluginBase
 {
     public function registerComponents()
     {
+        return [
+            'LukeNoble\Philter\Components\RecentImages' => 'RecentImages',
+            'LukeNoble\Philter\Components\UserImages' => 'UserImages',
+            'LukeNoble\Philter\Components\AddImages' => 'AddImages',
+            'LukeNoble\Philter\Components\DeleteImage' => 'DeleteImage',
+
+
+        ];
     }
 
     public function registerSettings()
---
+++ plugins/lukenoble/philter/controllers/Images.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LukeNoble\Philter\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class Images extends Controller
+{
+    public $implement = ['Backend\Behaviors\ListController',        'Backend\Behaviors\FormController',        'Backend\Behaviors\ReorderController'];
+
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
+    public $reorderConfig = 'config_reorder.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('LukeNoble.Philter', 'main-menu-item');
+    }
+}
